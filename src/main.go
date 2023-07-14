7c486243b537578b9537865edef06ba7dd465353
Y.Akazawa <yoshi_akazawa_sto@RED-iMac-3.local>
2023-07-14 23:50:13 +0900

    修正

--- src/main.go
+++ src/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/joho/godotenv"
 	"github.com/mitchellh/mapstructure"
+	"log"
+	"os"
 
 	"github.com/kemper0530/demo-backend/src/domain"
 	"github.com/kemper0530/demo-backend/src/infrastructure"
@@ -25,11 +29,56 @@ type NuxtMailArgs struct {
 	UpdatedAt string `mapstructure:"updatedat"`
 }
 
+var i *int
+var from, to, sub, body, prompt *string
+
 func main() {
-	lambda.Start(HandleRequests)
+	if os.Getenv("GO_ENV") == "production" {
+		fmt.Println("Starting production mode...")
+		lambda.Start(HandleRequests)
+	} else {
+		fmt.Println("Starting development mode...")
+		loadEnvVars()
+		initArgs()
+
+		// ローカルでテストするためのダミーのAppSyncEventを作成
+		var testEvent AppSyncEvent
+
+		switch *i {
+		case 0:
+			testEvent = AppSyncEvent{
+				OperationName: "createNuxtMail",
+				Arguments: map[string]interface{}{
+					"from":      *from,
+					"to":        *to,
+					"subject":   *sub,
+					"body":      *body,
+					"createdAt": "",
+					"updatedAt": "",
+				},
+			}
+		case 1:
+			testEvent = AppSyncEvent{
+				OperationName: "createChatGpt",
+				Arguments: map[string]interface{}{
+					"prompt": *prompt,
+				},
+			}
+		default:
+			log.Fatal("Invalid mode flag")
+		}
+
+		result, err := HandleRequests(context.Background(), testEvent)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println(result)
+	}
 }
 
 func HandleRequests(ctx context.Context, request AppSyncEvent) (interface{}, error) {
+	fmt.Printf("Request received: %+v\n", request) // デバッグログ
 	switch request.OperationName {
 	case "createChatGpt":
 		var args domain.ChatGpt
@@ -54,11 +103,30 @@ func HandleRequests(ctx context.Context, request AppSyncEvent) (interface{}, err
 }
 
 func handleNuxtMail(dnm domain.NuxtMail) (domain.Res, error) {
+	fmt.Printf("Handling NuxtMail: %+v\n", dnm) // デバッグログ
 	NuxtMailController := controllers.NewNuxtMailController(infrastructure.NewSES())
 	return NuxtMailController.SendSESEmail(dnm)
 }
 
 func handleChatGpt(dnm domain.ChatGpt) (domain.Res, error) {
+	fmt.Printf("Handling ChatGpt: %+v\n", dnm) // デバッグログ
 	ChatGptController := controllers.NewChatGptController(infrastructure.NewGPT())
 	return ChatGptController.SendChatGptPrompt(dnm)
+}
+
+func initArgs() {
+	i = flag.Int("i", 0, "mode flag(0:SESメール,1:ChatGpt)")
+	from = flag.String("f", os.Getenv("AWS_SES_TEST_FROM"), "SES から送信するメッセージの MAIL FROM ドメイン")
+	to = flag.String("t", os.Getenv("AWS_SES_TEST_SUCCESS_TO"), "SES から送信するメッセージの MAIL TO ドメイン")
+	sub = flag.String("s", "テスト件名", "メールの件名")
+	body = flag.String("b", "テスト本文", "メールの本文")
+	prompt = flag.String("p", "Go言語の特徴について教えて下さい.", "ChatGptのprompt")
+	flag.Parse()
+}
+
+func loadEnvVars() {
+	err := godotenv.Load(fmt.Sprintf("src/infrastructure/%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
